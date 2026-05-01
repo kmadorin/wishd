f7c43d6e7c5a62d87a71176769327ce2bcd724a0
Kirill Madorin <madorinkv@gmail.com>
2026-05-01 13:26:03 +0200

    feat(plugin-compound-v3): sepolia addresses + abi fragments

---
+++ plugins/compound-v3/abis/comet.ts
@@ -0,0 +1,40 @@
+export const cometAbi = [
+  {
+    type: "function",
+    name: "supply",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "asset", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [],
+  },
+  {
+    type: "function",
+    name: "supplyTo",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "dst", type: "address" },
+      { name: "asset", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [],
+  },
+  {
+    type: "function",
+    name: "allow",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "manager", type: "address" },
+      { name: "isAllowed", type: "bool" },
+    ],
+    outputs: [],
+  },
+  {
+    type: "function",
+    name: "balanceOf",
+    stateMutability: "view",
+    inputs: [{ name: "account", type: "address" }],
+    outputs: [{ type: "uint256" }],
+  },
+] as const;
